52716c425de82a64b3b4179d728590f0d0d275a6
abdelrahmman97 <mabdelrahmman22@gmail.com>
2024-04-19 18:55:07 +0200

    added Localization & toaster

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient } from '@angular/common/http';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { ToastrModule } from 'ngx-toastr';
+
+export function HttpLoaderFactory ( http: HttpClient ) {
+	return new TranslateHttpLoader( http );
+}
+
+@NgModule( {
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		AppRoutingModule,
+		ToastrModule.forRoot(),
+		TranslateModule.forRoot({
+			loader:{
+				provide: TranslateLoader,
+				useFactory: HttpLoaderFactory,
+				deps: [HttpClient]
+			}
+		}),
+	],
+	providers: [],
+	bootstrap: [ AppComponent ]
+} )
 export class AppModule { }
